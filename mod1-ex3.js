198597fd01cc4c98c9db486e92bf0598c93e8503
Karen Cardoso <kah.card@gmail.com>
2020-09-28 20:18:05 -0300

    Add ex3 and ex4

---
+++ mod1-ex3.js
@@ -0,0 +1,12 @@
+function temHabilidade(skills){
+
+    if(skills.indexOf("Javascript") >= 0){
+        console.log(true)
+    }  
+    else {
+        console.log(false)
+    }  
+}
+var skills = ["Javascript" , "ReactJS", "React Native"]
+
+temHabilidade(skills);
---
+++ mod1-ex4.js
@@ -0,0 +1,11 @@
+function experiencia(anos) {
+    switch(anos){
+       if(anos )
+    }
+   }
+   var anosEstudo = 7;
+   experiencia(anosEstudo);
+   // De 0-1 ano: Iniciante
+   // De 1-3 anos: Intermediário
+   // De 3-6 anos: Avançado
+   // De 7 acima: Jedi Master
